19129bdb24df0fd67bcdca806e1a70b78213c760
Meaghan Campbell <emeaghancampbell@gmail.com>
2021-02-22 20:27:53 -0600

    initial app

--- server.js
+++ server.js
@@ -36,7 +36,12 @@ function filterByQuery(query, animalsArray) {
     }
     // return the filtered results:
     return filteredResults;
-  }
+}
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -45,6 +50,16 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+// new get route using param instead of querys
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
 
 // tell app to listen for requests
 app.listen(PORT, () => {
